0254d465aa322a8e165a9b2cb3158b9b806dbb47
jonny <jonnywatson0886@gmail.com>
2025-05-06 17:05:38 +0100

    added in DP to the app allowing for single web app navigation

---
+++ src/app/details/details.component.ts
@@ -0,0 +1,46 @@
+import {Component, inject} from '@angular/core';
+import {CommonModule} from '@angular/common';
+import {ActivatedRoute} from '@angular/router';
+import {HousingService} from '../housing.service';
+import {HousingLocation} from '../housinglocation';
+
+
+@Component({
+  selector: 'app-details',
+  standalone: true,
+  imports: [CommonModule],
+  template: `
+    <article>
+      <img 
+      class="listing-photo"
+      [src]="housingLocation?.photo"
+      alt="Exterior photo of {{ housingLocation?.name }}"
+      crossorigin 
+      />
+    <section class = "listing-features">
+      <h2 class="section-heading">About this housing location</h2>
+      <ul>
+      <li>Units available: {{ housingLocation?.availableUnits }}</li>
+      <li>Does this location have wifi: {{ housingLocation?.wifi }}</li>
+      <li>Does this location have laundry: {{ housingLocation?.laundry }}</li>
+      </ul>
+    </section>
+    <section class = "listings-apply">
+      <h2 class = "section-heading">Apply to live here</h2>
+      <button class="primary" type = "button">Apply now</button>
+    </section>
+    </article>
+  `,
+  styleUrl: './details.component.css'
+})
+export class DetailsComponent {
+  
+  route: ActivatedRoute = inject(ActivatedRoute);
+  housingService = inject(HousingService);
+  housingLocation: HousingLocation | undefined;
+  
+  constructor() {
+    const housingLocationId = Number(this.route.snapshot.params['id']);
+    this.housingLocation = this.housingService.getHousingLocationById(housingLocationId);
+  }
+}
