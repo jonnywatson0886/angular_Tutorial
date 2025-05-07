7deae0e4ee7e2fc00bace1002e1aa922510e32b5
jonny <jonnywatson0886@gmail.com>
2025-05-07 09:46:01 +0100

    updated base url for images to allow images to work

--- src/app/housing.service.ts
+++ src/app/housing.service.ts
@@ -18,8 +18,8 @@ export class HousingService {
   }
   
 
-  readonly baseUrl = 'https://angular.io/assets/images/tutorials/faa';
-  
+  readonly baseUrl = 'https://angular.dev/assets/images/tutorials/common';
+
   housingLocationList: HousingLocation[] = [
     {
       id: 0,
@@ -29,7 +29,7 @@ export class HousingService {
       photo: `${this.baseUrl}/bernard-hermant-CLKGGwIBTaY-unsplash.jpg`,
       availableUnits: 4,
       wifi: true,
-      laundry: true
+      laundry: true,
     },
     {
       id: 1,
@@ -39,7 +39,7 @@ export class HousingService {
       photo: `${this.baseUrl}/brandon-griggs-wR11KBaB86U-unsplash.jpg`,
       availableUnits: 0,
       wifi: false,
-      laundry: true
+      laundry: true,
     },
     {
       id: 2,
@@ -49,7 +49,7 @@ export class HousingService {
       photo: `${this.baseUrl}/i-do-nothing-but-love-lAyXdl1-Wmc-unsplash.jpg`,
       availableUnits: 1,
       wifi: false,
-      laundry: false
+      laundry: false,
     },
     {
       id: 3,
@@ -59,7 +59,7 @@ export class HousingService {
       photo: `${this.baseUrl}/ian-macdonald-W8z6aiwfi1E-unsplash.jpg`,
       availableUnits: 1,
       wifi: true,
-      laundry: false
+      laundry: false,
     },
     {
       id: 4,
@@ -69,7 +69,7 @@ export class HousingService {
       photo: `${this.baseUrl}/krzysztof-hepner-978RAXoXnH4-unsplash.jpg`,
       availableUnits: 1,
       wifi: true,
-      laundry: false
+      laundry: false,
     },
     {
       id: 5,
@@ -79,7 +79,7 @@ export class HousingService {
       photo: `${this.baseUrl}/r-architecture-JvQ0Q5IkeMM-unsplash.jpg`,
       availableUnits: 2,
       wifi: true,
-      laundry: true
+      laundry: true,
     },
     {
       id: 6,
@@ -89,7 +89,7 @@ export class HousingService {
       photo: `${this.baseUrl}/phil-hearing-IYfp2Ixe9nM-unsplash.jpg`,
       availableUnits: 5,
       wifi: true,
-      laundry: true
+      laundry: true,
     },
     {
       id: 7,
@@ -99,7 +99,7 @@ export class HousingService {
       photo: `${this.baseUrl}/r-architecture-GGupkreKwxA-unsplash.jpg`,
       availableUnits: 2,
       wifi: true,
-      laundry: true
+      laundry: true,
     },
     {
       id: 8,
@@ -109,7 +109,7 @@ export class HousingService {
       photo: `${this.baseUrl}/saru-robert-9rP3mxf8qWI-unsplash.jpg`,
       availableUnits: 10,
       wifi: false,
-      laundry: false
+      laundry: false,
     },
     {
       id: 9,
@@ -119,8 +119,8 @@ export class HousingService {
       photo: `${this.baseUrl}/webaliser-_TPTXZd9mOo-unsplash.jpg`,
       availableUnits: 6,
       wifi: true,
-      laundry: true
-    }
+      laundry: true,
+    },
   ];
 
 }
