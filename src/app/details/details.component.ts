db74ae907ae354559658787c690e07e358ab573e
jonny <jonnywatson0886@gmail.com>
2025-05-07 10:02:25 +0100

    added form to capture user inputs and applied template for function to use that passed in data in the details component

--- src/app/details/details.component.ts
+++ src/app/details/details.component.ts
@@ -3,12 +3,13 @@ import {CommonModule} from '@angular/common';
 import {ActivatedRoute} from '@angular/router';
 import {HousingService} from '../housing.service';
 import {HousingLocation} from '../housinglocation';
+import { FormControl, FormGroup, ReactiveFormsModule } from '@angular/forms';
 
 
 @Component({
   selector: 'app-details',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, ReactiveFormsModule],
   template: `
     <article>
       <img 
@@ -27,20 +28,45 @@ import {HousingLocation} from '../housinglocation';
     </section>
     <section class = "listings-apply">
       <h2 class = "section-heading">Apply to live here</h2>
-      <button class="primary" type = "button">Apply now</button>
+      <form [formGroup]="applyForm" (submit)="submitApplication()"> 
+        <label for="first-Name">First Name</label>
+        <input id="first-Name" type = "text" formControlName = "firstName">
+
+        <label for="last-Name">Last Name</label>
+        <input id="last-Name" type = "text" formControlName = "lastName">
+        
+        <label for="email">Email</label>
+        <input id="email" type = "email" formControlName = "email">
+
+        <button type= "submit" class="primary"> Apply Now </button>
+      </form>
     </section>
     </article>
   `,
   styleUrl: './details.component.css'
 })
 export class DetailsComponent {
-  
-  route: ActivatedRoute = inject(ActivatedRoute);
-  housingService = inject(HousingService);
   housingLocation: HousingLocation | undefined;
-  
+
+  route: ActivatedRoute = inject(ActivatedRoute);
+  housingService = inject(HousingService);  
+  applyForm = new FormGroup({
+      firstName: new FormControl(''),
+      lastName: new FormControl(''),
+      email: new FormControl('')
+  });
+
   constructor() {
     const housingLocationId = Number(this.route.snapshot.params['id']);
     this.housingLocation = this.housingService.getHousingLocationById(housingLocationId);
+  }
+
+  submitApplication()
+  {
+    this.housingService.submitApplication(
+      this.applyForm.value.firstName ?? '',
+      this.applyForm.value.lastName ?? '',
+      this.applyForm.value.email ?? ''
+    )
   }
 }
--- src/app/housing.service.ts
+++ src/app/housing.service.ts
@@ -17,6 +17,11 @@ export class HousingService {
     return this.housingLocationList.find(housingLocation => housingLocation.id === id);
   }
   
+  submitApplication(firstName: string, lastName: string, email: string)
+  {
+    console.log(firstName,lastName,email);
+  }
+
 
   readonly baseUrl = 'https://angular.dev/assets/images/tutorials/common';
 
